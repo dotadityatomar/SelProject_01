9084301a313db3038773bd8a3db3b4a6eb6936b7
Aditya_Tomar <tomaradi143@gmail.com>
2025-09-15 11:40:48 +0530

    Of pro

--- main.py
+++ main.py
@@ -1,14 +1,22 @@
+import time
+
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+
 
 
 # Initialize WebDriver
 
-#driver = webdriver.Chrome(executable_path=r'C:\chromedriver-win64\chromedriver.exe')
 service = Service(r'C:\chromedriver-win64\chromedriver.exe')
+
 # Go to a website
-IP = int(input("Enter IP "))
-ht = "https://"
-IPHT = '"'+ht+IP+'"'
+
+
 driver= webdriver.Chrome(service=service)
-driver.get(IPHT)
+driver.implicitly_wait(4)
+driver.get("https://automationexercise.com/")
+driver.find_element(By.CLASS_NAME,"fa fa-angle-right").click()
+time.sleep(10)
+
